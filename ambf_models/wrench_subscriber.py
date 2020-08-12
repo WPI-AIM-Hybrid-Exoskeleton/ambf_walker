07e9064e4143754ecbe75a4cb29d2bcf47d9921f
CSBales <charlessbales@gmail.com>
2020-08-12 06:55:07 -0400

    additional data grabber file

--- ambf_models/wrench_subscriber.py
+++ ambf_models/wrench_subscriber.py
@@ -7,100 +7,31 @@ from ambf_msgs.msg import RigidBodyState
 class Grabber:
 
     def __init__(self):
-        self.
-        self.
-        self.
+        self.FrontSensorLeftThigh = None
+        self.FrontSensorLeftShank = None
+        self.FrontSensorRightThigh = None
+        self.FrontSensorRightShank = None
+        self.BackSensorLeftThigh = None
+        self.BackSensorLeftShank = None
+        self.BackSensorRightThigh = None
+        self.BackSensorRightShank = None
+        self.SensorLeftFoot1 = None
+        self.SensorLeftFoot2 = None
+        self.SensorLeftFoot3 = None
+        self.SensorRightFoot1 = None
+        self.SensorRightFoot2 = None
+        self.SensorRightFoot3 = None
 
-    def callback00(data):
+    def callback_leg(data):
 
         print('FrontSensorLeftThigh')
         print(data.wrench)
 
-    def callback01(data):
+    def callback_foot(data):
 
         print('FrontSensorLeftShank')
         print(data.wrench)
 
-    def callback02(data):
-
-        print('FrontSensorRightThigh')
-        print(data.wrench)
-
-    def callback03(data):
-
-        print('FrontSensorRightShank')
-        print(data.wrench)
-
-    def callback04(data):
-
-        print('BackSensorLeftThigh')
-        print(data.wrench)
-
-    def callback05(data):
-
-        print('BackSensorLeftShank')
-        print(data.wrench)
-
-    def callback06(data):
-
-        print('BackSensorRightThigh')
-        print(data.wrench)
-
-    def callback07(data):
-
-        print('BackSensorRightShank')
-        print(data.wrench)
-
-    def callback08(data):
-
-        print('SensorLeftFoot1')
-        print(data.wrench)
-
-    def callback09(data):
-
-        print('SensorLeftFoot2')
-        print(data.wrench)
-
-    def callback10(data):
-
-        print('SensorLeftFoot3')
-        print(data.wrench)
-
-    def callback11(data):
-
-        print('SensorRightFoot1')
-        print(data.wrench)
-
-    def callback12(data):
-
-        print('SensorRightFoot2')
-        print(data.wrench)
-
-    def callback13(data):
-
-        print('SensorRightFoot3')
-        print(data.wrench)
-
-    def wrench_subscriber():
-
-    rospy.init_node('wrench_subscriber', anonymous=True)
-
-    rospy.Subscriber("/ambf/env/FrontSensorLeftThigh/State", RigidBodyState, callback00)
-    rospy.Subscriber("/ambf/env/FrontSensorLeftShank/State", RigidBodyState, callback01)
-    rospy.Subscriber("/ambf/env/FrontSensorRightThigh/State", RigidBodyState, callback02)
-    rospy.Subscriber("/ambf/env/FrontSensorRightShank/State", RigidBodyState, callback03)
-    rospy.Subscriber("/ambf/env/BackSensorLeftThigh/State", RigidBodyState, callback04)
-    rospy.Subscriber("/ambf/env/BackSensorLeftShank/State", RigidBodyState, callback05)
-    rospy.Subscriber("/ambf/env/BackSensorRightThigh/State", RigidBodyState, callback06)
-    rospy.Subscriber("/ambf/env/BackSensorRightShank/State", RigidBodyState, callback07)
-    rospy.Subscriber("/ambf/env/SensorLeftFoot1/State", RigidBodyState, callback08)
-    rospy.Subscriber("/ambf/env/SensorLeftFoot2/State", RigidBodyState, callback09)
-    rospy.Subscriber("/ambf/env/SensorLeftFoot3/State", RigidBodyState, callback10)
-    rospy.Subscriber("/ambf/env/SensorRightFoot1/State", RigidBodyState, callback11)
-    rospy.Subscriber("/ambf/env/SensorRightFoot2/State", RigidBodyState, callback12)
-    rospy.Subscriber("/ambf/env/SensorRightFoot3/State", RigidBodyState, callback13)
-
-    rospy.spin()
 
 if __name__ == '__main__':
     try:
@@ -108,6 +39,27 @@ if __name__ == '__main__':
 
         grabber = Grabber()
 
-        wrench_subscriber()
+        rospy.Subscriber("/ambf/env/FrontSensorLeftThigh/State", RigidBodyState, grabber.callback_leg)
+        rospy.Subscriber("/ambf/env/BackSensorLeftThigh/State", RigidBodyState, grabber.callback_leg)
+
+        rospy.Subscriber("/ambf/env/FrontSensorLeftShank/State", RigidBodyState, grabber.callback_leg)
+        rospy.Subscriber("/ambf/env/BackSensorLeftShank/State", RigidBodyState, grabber.callback_leg)
+
+        rospy.Subscriber("/ambf/env/FrontSensorRightThigh/State", RigidBodyState, grabber.callback_leg)
+        rospy.Subscriber("/ambf/env/BackSensorRightThigh/State", RigidBodyState, grabber.callback_leg)
+
+        rospy.Subscriber("/ambf/env/FrontSensorRightShank/State", RigidBodyState, grabber.callback_leg)
+        rospy.Subscriber("/ambf/env/BackSensorRightShank/State", RigidBodyState, grabber.callback_leg)
+       
+        rospy.Subscriber("/ambf/env/SensorLeftFoot1/State", RigidBodyState, grabber.callback_foot)
+        rospy.Subscriber("/ambf/env/SensorLeftFoot2/State", RigidBodyState, grabber.callback_foot)
+        rospy.Subscriber("/ambf/env/SensorLeftFoot3/State", RigidBodyState, grabber.callback_foot)
+        
+        rospy.Subscriber("/ambf/env/SensorRightFoot1/State", RigidBodyState, grabber.callback_foot)
+        rospy.Subscriber("/ambf/env/SensorRightFoot2/State", RigidBodyState, grabber.callback_foot)
+        rospy.Subscriber("/ambf/env/SensorRightFoot3/State", RigidBodyState, grabber.callback_foot)
+
+        rospy.spin()
+
     except rospy.ROSInterruptException:
         pass  
